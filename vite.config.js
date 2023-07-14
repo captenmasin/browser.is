2e1d3091b3ff918fb0e86ed0b9b1cee04cfc1bdc
Mason Day <mason@speed.agency>
2023-07-14 15:38:31 +0100

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,26 @@
 import path from 'path'
 import {defineConfig} from 'vite';
-import {run} from 'vite-plugin-run'
 import laravel from 'laravel-vite-plugin';
 import vuePlugin from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve('./resources/js'),
-            'tailwind.config.js': path.resolve(__dirname, 'tailwind.config.js'),
-        }
-    },
-    optimizeDeps: {
-        include: [
-            'tailwind.config.js',
-        ]
-    },
-    plugins: [
-        vuePlugin({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            }
-        }),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        run([{}]),
-    ],
+	resolve: {
+		alias: {
+			'@': path.resolve('./resources/js'),
+		}
+	},
+	plugins: [
+		vuePlugin({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false,
+				},
+			}
+		}),
+		laravel({
+			input: ['resources/css/app.css', 'resources/js/app.js'],
+			refresh: true,
+		}),
+	],
 });
