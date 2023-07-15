83a340d087c0cf8638140c2eb050529c43897862
Mason Day <mason@speed.agency>
2023-07-15 17:36:40 +0100

    WIP

--- vite.config.js
+++ vite.config.js
@@ -22,5 +22,20 @@ export default defineConfig({
 			input: ['resources/css/app.css', 'resources/js/app.js'],
 			refresh: true,
 		}),
-	]
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id
+							.toString()
+							.split('node_modules/')[1]
+							.split('/')[0]
+							.toString();
+					}
+				},
+			},
+		},
+	}
 });
