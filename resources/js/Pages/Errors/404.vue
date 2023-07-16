e941b7783c559de677543e0f9e83f95517ed3616
Mason Day <mason@speed.agency>
2023-07-16 19:08:52 +0100

    WIP

--- resources/js/Pages/Errors/404.vue
+++ resources/js/Pages/Errors/404.vue
@@ -1,10 +1,12 @@
 <template>
-<div class="flex flex-col items-center justify-center py-24">
-    <h1 class="text-8xl font-heading">
-        404
-    </h1>
-    <h2 class="text-primary text-3xl">
-        Page not found
-    </h2>
-</div>
+    <div class="flex flex-col items-center justify-center">
+        <div class="items-center flex-col text-center mt-12">
+            <h1 class="text-[12rem] font-heading">
+                404
+            </h1>
+            <h2 class="text-primary text-3xl">
+                Page not found
+            </h2>
+        </div>
+    </div>
 </template>
