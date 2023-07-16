27f59f42ed6c8db74dfea99236e57aa50542dc57
Mason Day <mason@speed.agency>
2023-07-16 17:39:05 +0100

    WIP

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 @include('partials.meta.head')
-<body class="font-body bg-tertiary text-secondary bg-cover antialiased dark:bg-gray-900">
+<body class="font-body bg-tertiary text-secondary antialiased dark:bg-gray-900">
 <div id="modals" class="relative z-20 flex justify-center"></div>
 @inertia
 @vite('resources/js/app.js')
